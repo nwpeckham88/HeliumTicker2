46f479501903c40e3853af8620145bcfc1087070
nwpeckham88 <74386527+nwpeckham88@users.noreply.github.com>
2021-12-02 05:10:52 -0600

    Includes

--- HeliumTicker2.ino
+++ HeliumTicker2.ino
@@ -1,3 +1,23 @@
+// Basic ESP8266 wifi functions
+#include <ESP8266WiFi.h>
+// DNS Server
+#include <DNSServer.h>
+
+//Async WebServer 
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+//Async WebClient
+#include <AsyncHTTPRequest_Generic.h>           // https://github.com/khoih-prog/AsyncHTTPRequest_Generic
+
+// Event scheduling
+#include <Ticker.h>
+
+#include <NeoPixelBus.h>
+
+#define ESP_RESET ESP.reset()
+
+
 void setup() {
   // put your setup code here, to run once:
 
